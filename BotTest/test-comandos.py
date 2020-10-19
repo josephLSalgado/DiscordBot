4639d26211d3fa8ebcaccd12deff0ef69ab4e707
josephLSalgado <49181840+josephLSalgado@users.noreply.github.com>
2020-10-18 23:05:56 -0500

    Nuevo comando con embed

--- BotTest/test-comandos.py
+++ BotTest/test-comandos.py
@@ -1,4 +1,5 @@
 import discord
+import datetime
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='.', description='Este es un bot servicial :)')
@@ -10,10 +11,29 @@ bot = commands.Bot(command_prefix='.', description='Este es un bot servicial :)'
 async def saludo(ctx):
     await ctx.send('Hola Spartan!')
 
+#Información sobre la beta de Cold War
+@bot.command()
+async def cw(ctx):
+    fechaFinal = datetime.datetime.strptime('19/10/2020', '%d/%m/%Y')
+    hoy = datetime.datetime.today()
+    tiempoRestante = (fechaFinal - hoy).total_seconds()/3600
+
+    if int(tiempoRestante) > 0:
+        embed = discord.Embed(title=f'{ctx.guild.name}',
+            description=f'La beta de Cold War terminará en: {int(tiempoRestante)} horas. \nLlega al nivel 10 de la beta para recibir Proyecto de subfusil excepcional.')
+
+        embed.set_image(url='https://teamdarkpirates.com/wp-content/uploads/2019/07/Subfusil-Fortnite-r%C3%A1fagas-banner.jpg')
+
+    else:
+        embed = discord.Embed(title=f'{ctx.guild.name}',
+            description='La beta de Cold War ha terminado.')
+
+    await ctx.send(embed=embed)
+
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(
-            name='Halo'))
+            name='Beta de Cold War con el FrnK'))
     print('El bot está listo.')
 
 #NO OLVIDES de poner el TOKEN :)
